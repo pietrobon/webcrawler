ddf29ce58f4ed4dc9dc9c66dbfbc58516558b314
pietrobon <vicentespietrobon@gmail.com>
2023-09-30 17:14:17 -0300

    last update

--- src/crawler.py
+++ src/crawler.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import datetime
 
-class Cralwer:
+class Crawler:
 
     def __init__(self):
         self.dolar_data = None
@@ -15,10 +15,13 @@ class Cralwer:
 
     def extract_from_dolar_hoje(self):
         raw_dolar = self.request_data('https://dolarhoje.com/')
+        
         # Find Currency and price element
         currency_element = raw_dolar.find('span', {'class': 'cotMoeda nacional'})
+        
         if currency_element:
             input_element = currency_element.find('input', {'id': 'nacional'})
+            
             if input_element:
                 value = input_element.get('value')
                 value = value.replace(',', '.')
@@ -31,14 +34,18 @@ class Cralwer:
                 print("Input element not found.")
         else:
             print("Currency element not found.")
+        
         print(self.dolar_data)
 
     def extract_from_euro_hoje(self):
         raw_euro = self.request_data('https://dolarhoje.com/euro-hoje/')
+        
         # Find Currency and price element
         currency_element = raw_euro.find('span', {'class': 'cotMoeda nacional'})
+        
         if currency_element:
             input_element = currency_element.find('input', {'id': 'nacional'})
+            
             if input_element:
                 value = input_element.get('value')
                 value = value.replace(',', '.')
@@ -51,9 +58,10 @@ class Cralwer:
                 print("Input element not found.")
         else:
             print("Currency element not found.")
-        print(self.euro_data)
         
+        print(self.euro_data)
+
 if __name__ == "__main__":
-    crawler = Cralwer()
+    crawler = Crawler()
     crawler.extract_from_dolar_hoje()
     crawler.extract_from_euro_hoje()
